9acd4c61a37657fd6b61399fee80d3c1fcf8eb23
Joshua Lynch <josh@joshua-lynch.com>
2011-08-30 00:02:55 -0700

    Cleaned up spacing, changed language prompts

--- comments.php
+++ comments.php
@@ -15,87 +15,132 @@ function html5_comment( $comment, $args, $depth ) {
 	switch ( $comment->comment_type ) :
 		case '' :
 	?>
+	
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
+			
 			<footer>
+				
 				<div class="comment-author vcard">
-					<?php echo get_avatar( $comment, 40 ); ?>
+					
+					<?php echo get_avatar( $comment, 42 ); ?>
+					
 					<?php printf( __( '%s <span class="says">says:</span>', 'html5' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-				</div><!-- .comment-author .vcard -->
+				
+				</div>
+				
 				<?php if ( $comment->comment_approved == '0' ) : ?>
+				
 					<em><?php _e( 'Your comment is awaiting moderation.', 'html5' ); ?></em>
 					<br />
+				
 				<?php endif; ?>
 
 				<div class="comment-meta commentmetadata">
-					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
-					<?php
-						/* translators: 1: date, 2: time */
-						printf( __( '%1$s at %2$s', 'html5' ), get_comment_date(),  get_comment_time() ); ?>
-					</time></a>
-					<?php edit_comment_link( __( '(Edit)', 'html5' ), ' ' );
-					?>
-				</div><!-- .comment-meta .commentmetadata -->
+					
+					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+					
+						<time pubdate datetime="<?php comment_time( 'c' ); ?>">
+					
+						<?php printf( __( '%1$s at %2$s', 'html5' ), get_comment_date(),  get_comment_time() ); ?>
+					
+						</time>
+						
+					</a>
+					
+					<?php edit_comment_link( __( '(Edit)', 'html5' ), ' ' );?>
+					
+				</div>
+			
 			</footer>
 
 			<div class="comment-content"><?php comment_text(); ?></div>
 
 			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
-		</article><!-- #comment-##  -->
+				
+				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => 'Respond', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+			
+			</div>
+			
+		</article>
 
-	<?php
-			break;
+	<?php 
+	
+		break;
+		
 		case 'pingback'  :
+		
 		case 'trackback' :
+	
 	?>
+	
 	<li class="post pingback">
+	
 		<p><?php _e( 'Pingback:', 'html5' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'html5'), ' ' ); ?></p>
+	
 	<?php
-			break;
-	endswitch;
-}
-endif; // ends check for html5_comment()
+			
+		break;
+	
+		endswitch;
 
-?>
+}
+
+		endif; // ends check for html5_comment()
+
+	?>
 
 	<div id="comments">
-	<?php if ( post_password_required() ) : ?>
-		<div class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'html5' ); ?></div>
-	</div><!-- .comments -->
+	
+		<?php if ( post_password_required() ) : ?>
+		
+			<p class="no-password"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'html5' ); ?></p>
+	
+	</div>
+	
 	<?php return;
 		endif;
 	?>
 
-	<?php // You can start editing here -- including this comment! ?>
+	<?php // You can start editing here ?>
 
 	<?php if ( have_comments() ) : ?>
+		
 		<h2 id="comments-title">
-			<?php
-			    printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'html5' ),
-			        number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
-			?>
+			
+			<?php printf( _n( 'Comments (1)', 'Comments (%1$s)', get_comments_number(), 'html5' ), number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' ); ?>
+		
 		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above">
-			<h1 class="section-heading"><?php _e( 'Comment navigation', 'html5' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'html5' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'html5' ) ); ?></div>
-		</nav>
+		
+			<nav id="pagination" role="navigation">
+		
+				<div class="older"><?php previous_comments_link( __( 'Older Comments', 'html5' ) ); ?></div>
+				
+				<div class="newer"><?php next_comments_link( __( 'Newer Comments', 'html5' ) ); ?></div>
+			
+			</nav>
+			
 		<?php endif; // check for comment navigation ?>
 
 		<ol class="commentlist">
+		
 			<?php wp_list_comments( array( 'callback' => 'html5_comment' ) ); ?>
+		
 		</ol>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below">
-			<h1 class="section-heading"><?php _e( 'Comment navigation', 'html5' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'html5' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'html5' ) ); ?></div>
-		</nav>
+		
+			<nav id="pagination" role="navigation">
+		
+				<div class="older"><?php previous_comments_link( __( 'Older Comments', 'html5' ) ); ?></div>
+				
+				<div class="newer"><?php next_comments_link( __( 'Newer Comments', 'html5' ) ); ?></div>
+			
+			</nav>
+			
 		<?php endif; // check for comment navigation ?>
 
 	<?php else : // this is displayed if there are no comments so far ?>
@@ -104,20 +149,18 @@ endif; // ends check for html5_comment()
 
 		<?php else : // or, if we don't have comments:
 
-			/* If there are no comments and comments are closed,
-			 * let's leave a little note, shall we?
-			 * But only on posts! We don't want the note on pages.
-			 */
-			if ( ! comments_open() && ! is_page() ) :
-			?>
-			<p class="nocomments"><?php _e( 'Comments are closed.', 'html5' ); ?></p>
+			// If no comments exist and comments are closed, leave a note
+			
+			if ( ! comments_open() && ! is_page() ) : ?>
+			
+				<h2 id="comments-title"><?php _e( 'Comments are closed.', 'html5' ); ?></h2>
+			
 			<?php endif; // end ! comments_open() && ! is_page() ?>
-
 
 		<?php endif; ?>
 
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+	<?php comment_form(array('comment_notes_after'=>'','title_reply'=>'Post a Comment')); ?>
 
-</div><!-- #comments -->
+</div>
